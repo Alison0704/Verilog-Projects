341fe4fdf8f8fba126be292d378f5c19151615a2
Alison Emilien <aemil072@uottawa.ca>
2025-01-28 22:03:08 -0500

    section 5 practice

---
+++ Section5/HA_bev/half_adder_behavioral.v
@@ -0,0 +1,42 @@
+module half_adder_behavioral(
+    input a, 
+    input b,
+    output reg sum,
+    output reg carry
+    );
+
+    always @(*) begin
+        sum = a ^ b;
+        carry = a & b;
+    end
+
+endmodule
+
+module testbench();
+
+reg a;
+reg b;
+wire sum;
+wire carry;
+
+half_adder_behavioral HALF_ADD(
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .carry(carry)
+);
+
+initial begin
+    $monitor("a=%b, b=%b, sum=%b, carry=%b", a, b, sum, carry);
+end
+
+initial begin
+    #1; a=0; b=0;
+    #1; a=0; b=1;
+    #1; a=1; b=0;
+    #1; a=1; b=1;
+    #1; a=0; b=0;
+    #1; a=1; b=1;
+end
+
+endmodule
---
+++ Section5/HA_dataflow/half_adder_dataflow.v
@@ -0,0 +1,40 @@
+module half_adder_dataflow(
+    input a,
+    input b, 
+    output sum,
+    output carry
+    );
+
+    assign sum = a ^ b;
+    assign carry = a & b;
+
+endmodule
+
+module testbench();
+
+reg a;
+reg b;
+wire sum;
+wire carry;
+
+half_adder_dataflow HALF_ADD(
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .carry(carry)
+);
+
+initial begin
+    $monitor("a=%b, b=%b, sum=%b, carry=%b", a, b, sum, carry);
+end
+
+initial begin
+    #1; a=0; b=0;
+    #1; a=0; b=1;
+    #1; a=1; b=0;
+    #1; a=1; b=1;
+    #1; a=0; b=0;
+    #1; a=1; b=1;
+end
+
+endmodule
